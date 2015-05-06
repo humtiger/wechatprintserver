f1098a6988661182f6bc47103c10c827955fd947
humtiger <humtiger@163.com>
2015-05-06 15:38:23 +0800

    create class

---
+++ src/org/chatprint/servlet/LocationServlet.java
@@ -0,0 +1,13 @@
+package org.chatprint.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * 
+ * 
+ */
+public class LocationServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+}
